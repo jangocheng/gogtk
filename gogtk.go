0d51518d84cf3ca5147a41eb2ee28eacb9b3dc62
熊建波 <i@89ge.cn>
2017-12-21 01:26:55 +0800

    界面代码

--- gogtk.go
+++ gogtk.go
@@ -1,8 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 )
-func main(){
-	fmt.Println("hello")
+
+func main() {
+	var outTE *walk.TextEdit
+	MainWindow{
+		Title:   "eztool",
+		MinSize: Size{400, 400},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &outTE, ReadOnly: true},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					PushButton{
+						Text:     "启动Nginx",
+						OnClicked: func() {
+						},
+					},
+					PushButton{
+						Text:      "启动redis",
+						OnClicked: func() { },
+					},
+					PushButton{
+						Text:      "一键安装composer",
+						OnClicked: func() { },
+					},
+					PushButton{
+						Text:      "一键安装Golang",
+						OnClicked: func() { },
+					},
+				},
+			},
+		},
+	}.Run()
 }
